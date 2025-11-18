5ef635c9e35ac0efdd0aabfd1beec4418b616817
benjaminjkern <benkern@pixelgate.net>
2025-11-17 20:39:46 -0800

    All fixed

--- src/App.tsx
+++ src/App.tsx
@@ -22,89 +22,92 @@ const App = () => {
                     height: "100vh",
                     "justify-content": "center",
                     "align-items": "center",
-                    "text-shadow": "2px 2px 5px black",
+                    // "text-shadow": "2px 2px 5px black",
                     color: "white",
                 }}
             >
                 <img
                     style={{
                         "border-radius": "50%",
-                        height: "175px",
+                        height: "300px",
                         width: "auto",
                         margin: "30px",
+                        border: "5px black solid",
                     }}
                     src="/linkedInPic.jpg"
                     alt="Me"
                 />
                 <h1
                     style={{
+                        "font-size": "150px",
+                    }}
+                >
+                    Hello!
+                </h1>
+                <span
+                    style={{
                         "font-size": "50px",
                     }}
                 >
-                    Benjamin Kern.
-                </h1>
-                <span>Nice to meet you.</span>
+                    I'm Ben!
+                </span>
 
                 <div
                     style={{
-                        "flex-direction": "row",
-                    }}
-                >
-                    <CircleLink
-                        href="https://www.linkedin.com/in/benjamin-j-kern/"
-                        icon="fa-linkedin"
-                    />
-                    <CircleLink
-                        href="https://www.github.com/benjaminjkern"
-                        icon="fa-github"
-                    />
-                    <CircleLink
-                        href="mailto:benkern@pixelgate?Subject=Hi Ben, you seem cool"
-                        icon="fa-envelope"
-                    />
-                </div>
-            </div>
-
-            <div
-                style={{
-                    height: "75px",
-                    background: "linear-gradient(#0000, #222)",
-                }}
-            />
-            <div
-                style={{
-                    color: "#a185c5",
-                    "text-align": "center",
-                    "background-color": "#222",
-                    padding: "50px 0",
-                    "align-items": "center",
-                }}
-            >
-                <h1 style={{ "margin-bottom": "10px" }}>About Me</h1>
-                <span>LMU Computer Science and Physics '20.</span>
-                <span>I do projects having to do with those things.</span>
-                <DynamicStyleComponent
-                    elementName="a"
-                    href="/resume"
-                    style={{
-                        "text-decoration": "none",
-                        padding: "5px",
-                        "border-radius": "5px",
-                        color: "black",
-                        margin: "20px",
-                        "max-width": "200px",
                         width: "100%",
-                        transition: "background-color 0.1s",
-                    }}
-                    dynamicStyle={{
-                        "background-color": "#a185c5",
-                        "&:hover": {
-                            backgroundColor: "#796496",
-                        },
+                        position: "absolute",
+                        bottom: 0,
+                        "align-items": "center",
+                        gap: "10px",
                     }}
                 >
-                    RESUMÉ
-                </DynamicStyleComponent>
+                    <div
+                        style={{
+                            "flex-direction": "row",
+                        }}
+                    >
+                        <CircleLink
+                            href="https://www.linkedin.com/in/benjamin-j-kern/"
+                            icon="fa-linkedin"
+                        />
+                        <CircleLink
+                            href="https://www.github.com/benjaminjkern"
+                            icon="fa-github"
+                        />
+                        <CircleLink
+                            href="mailto:hi@benjaminjkern.com?Subject=Hi Ben, you seem cool"
+                            icon="fa-envelope"
+                        />
+                    </div>
+                    <DynamicStyleComponent
+                        elementName="a"
+                        href="https://benjaminjkern.com/resume"
+                        style={{
+                            "text-decoration": "none",
+                            padding: "5px",
+                            "border-radius": "50px",
+                            color: "black",
+                            margin: "20px",
+                            "margin-top": "10px",
+                            "max-width": "190px",
+                            "font-size": "20px",
+                            "justify-content": "center",
+                            "align-items": "center",
+                            display: "flex",
+                            height: "50px",
+                            width: "100%",
+                            transition: "background-color 0.1s",
+                        }}
+                        dynamicStyle={{
+                            "background-color": "white",
+                            "&:hover": {
+                                backgroundColor: "#a185c5",
+                            },
+                        }}
+                    >
+                        Resumé
+                    </DynamicStyleComponent>
+                </div>
             </div>
         </>
     );
@@ -120,15 +123,14 @@ const CircleLink = (props: { href: string; icon: string }) => {
                 "justify-content": "center",
                 "align-items": "center",
                 "text-decoration": "none",
-                height: "52px",
-                width: "52px",
-                "font-size": "30px",
+                height: "50px",
+                width: "50px",
+                "font-size": "28px",
                 "border-radius": "50%",
                 color: "black",
                 "margin-right": "10px",
                 "margin-left": "10px",
-                "margin-top": "15px",
-                "text-shadow": "0px 0px 5px black",
+                // "text-shadow": "0px 0px 5px black",
                 transition: "background-color 0.1s",
             }}
             dynamicStyle={{
